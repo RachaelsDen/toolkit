bb21bf9977481741761fadf529bd222d06f013a3
Katelyn Goodwin <datateccs@gmail.com>
2026-09-19 19:15:57 -0700

    test: register issue-comment finding coverage

--- pr_guard/pr_guard_test.py
+++ pr_guard/pr_guard_test.py
@@ -1008,6 +1008,7 @@ from .pr_guard_repo import configure
 
 _SUITES = (
     "pr_guard_classify_test",
+    "pr_guard_issue_comments_test",
     "pr_guard_fnmatch_test",
     "pr_guard_fnmatch_hyphen_test",
     "pr_guard_fnmatch_reversed_test",
